c02b3fb88ba00dd530c4a3970b4d568d1f2b5650
Gerardo <geramoralesrico@gmail.com>
2025-01-21 21:51:23 -0600

    add constants and basic metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/assets/styles/globals.css'
-import { APP_NAME } from '@/lib/constants'
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: `${APP_NAME}`,
-  description: 'Ecommerce built with NextJS',
+  title: {
+    template: `%s | Demo Store`,
+    default: APP_NAME,
+  },
+  description: `${APP_DESCRIPTION}`,
+  metadataBase: new URL(SERVER_URL)
 }
 
 export default function RootLayout({
