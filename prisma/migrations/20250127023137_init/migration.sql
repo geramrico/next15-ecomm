305a7378288734a4ab6bcffd1adf4389f7dfa46d
Gerardo <geramoralesrico@gmail.com>
2025-01-26 20:45:54 -0600

    seed database

--- prisma/migrations/20250127023137_init/migration.sql
+++ prisma/migrations/20250127023137_init/migration.sql
@@ -10,7 +10,7 @@ CREATE TABLE "Product" (
     "stock" INTEGER NOT NULL,
     "price" DECIMAL(12,2) NOT NULL DEFAULT 0,
     "rating" DECIMAL(3,2) NOT NULL DEFAULT 0,
-    "numReview" INTEGER NOT NULL DEFAULT 0,
+    "numReviews" INTEGER NOT NULL DEFAULT 0,
     "isFeatured" BOOLEAN NOT NULL DEFAULT false,
     "banner" TEXT,
     "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
