c794ffc156c70833b745ab04eb9e781f4789afc8
Gerardo <geramoralesrico@gmail.com>
2025-02-03 07:50:45 -0600

    auth/add sample data for seeding db with users

--- db/seed.ts
+++ db/seed.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client'
 import sampleData from './sample-data'
 
+/* Run in terminal with: npx tsx ./db/seed */
+
 async function main() {
   const prisma = new PrismaClient()
+
+  //Reset DB
   await prisma.product.deleteMany()
+  await prisma.account.deleteMany()
+  await prisma.session.deleteMany()
+  await prisma.verificationToken.deleteMany()
+  await prisma.user.deleteMany()
 
   await prisma.product.createMany({ data: sampleData.products })
+  await prisma.user.createMany({ data: sampleData.users })
 
   console.log('DB Seeded succesfully')
 }
